350d7386c40203692a5931e94ecc9941c67ba24d
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2022-01-31 23:54:40 +0300

    Feat-UI: Static pages were added

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// Template Engine
+app.set("view engine", "ejs");
+
+// Middlewares
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.status(200).send("Index Page");
 });
