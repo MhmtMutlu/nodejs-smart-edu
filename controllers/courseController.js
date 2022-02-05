d6a3f711bde0c8c9898ed0064a308f7be2226b43
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2022-02-05 21:38:02 +0300

    Fix: courseController was edited

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,8 +1,8 @@
 const Course = require("../models/Course");
 
 exports.createCourse = async (req, res) => {
-  const course = await Course.create(req.body);
   try {
+    const course = await Course.create(req.body);
     res.status(201).json({
       status: "success",
       course,
