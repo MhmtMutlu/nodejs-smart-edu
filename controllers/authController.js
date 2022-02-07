761c91523d309ada897b607026271d64c02ae05e
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2022-02-08 00:58:01 +0300

    Feat: Login operations was created

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 exports.createUser = async (req, res) => {
   try {
@@ -7,6 +8,27 @@ exports.createUser = async (req, res) => {
       status: "success",
       user,
     });
+  } catch (error) {
+    res.status(400).json({
+      status: "error",
+      error,
+    });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    console.log(email, password);
+    const user = await User.findOne({ email: email });
+    if (user) {
+      bcrypt.compare(password, user.password, (err, same) => {
+        if (same) {
+          // SESSION
+          return res.status(200).send("YOU ARE LOGGED IN");
+        }
+      });
+    }
   } catch (error) {
     res.status(400).json({
       status: "error",
