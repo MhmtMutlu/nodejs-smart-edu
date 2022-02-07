751921ece2a178c5ddb687f6a8ae90bfcd52085c
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2022-02-07 23:06:24 +0300

    Feat: Routing operations were completed for register page

--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.route("/").get(pageController.getHomePage);
 router.route("/about").get(pageController.getAboutPage);
+router.route("/register").get(pageController.getRegisterPage);
 
 module.exports = router;
