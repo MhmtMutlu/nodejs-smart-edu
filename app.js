dec5676f149ef94bca95db5918a330a60a0dfa00
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2022-02-07 23:15:28 +0300

    Feat: Creating user operations were completed

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const pageRoute = require("./routes/pageRoute");
 const courseRoute = require("./routes/courseRoute");
 const categoryRoute = require("./routes/categoryRoute");
+const userRoute = require("./routes/userRoute");
 
 const app = express();
 const port = 3000;
@@ -29,6 +30,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/", pageRoute);
 app.use("/courses", courseRoute);
 app.use("/categories", categoryRoute);
+app.use("/users", userRoute);
 
 app.listen(port, () => {
   console.log(`App startred on port ${port}`);
