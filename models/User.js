181be6eb3568b70b79e5b4c2a18c5495a624f9ff
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2022-02-07 23:23:26 +0300

    Feat-Fix: User model was edited to hash password

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
@@ -15,6 +16,14 @@ const UserSchema = new Schema({
     type: String,
     required: true,
   },
+});
+
+UserSchema.pre("save", function (next) {
+  const user = this;
+  bcrypt.hash(user.password, 10, (err, hash) => {
+    user.password = hash;
+    next();
+  });
 });
 
 const User = mongoose.model("User", UserSchema);
